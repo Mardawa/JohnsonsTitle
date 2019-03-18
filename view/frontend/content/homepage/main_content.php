f30b622c5aca7fd178364c6c6dc7a5d82b995350
840Sh33p <luuquocbao97@gmail.com>
2019-03-18 15:01:25 +0100

    only showing 2 section in homepage

--- view/frontend/content/homepage/main_content.php
+++ view/frontend/content/homepage/main_content.php
@@ -2,9 +2,11 @@
 
     <div class="row">
 
+        <div class="col-md-3"></div>
+
         <div class="col-md-3">
             <h2 class="centered-text-image">
-                <a class="text-decoration-none text-white" href="index.php?action=clothing">Clothing </a>
+                <a class="text-decoration-none text-white" href="index.php?action=clothing"> Shop </a>
             </h2>
             <a href="index.php?action=clothing"> <img class="img-fluid card-img-top" src="img/generic2.jpg" alt="Abor Collective picture" /></a>
         </div>
@@ -16,7 +18,9 @@
             <a href="index.php?action=title"> <img class="img-fluid card-img-top text-white" src="img/generic4.jpg" alt="Abor Collective picture" /></a>
         </div>
 
-        <div class=" col-md-3">
+        <div class="col-md-3"></div>
+
+        <!-- <div class=" col-md-3">
             <h2 class="centered-text-image">
                 <a class="text-decoration-none text-white" href="#">Essentials</a>
             </h2>
@@ -29,7 +33,7 @@
                 <a class="text-decoration-none text-white" href="#"> Respect </a>
             </h2>
             <a href="#"> <img class="img-fluid card-img-top" src="img/generic3.jpg" alt="Abor Collective picture" /></a>
-        </div>
+        </div> -->
 
     </div>
 </div> 
